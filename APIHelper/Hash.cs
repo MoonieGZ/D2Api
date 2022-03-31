9004cffc0ac802fe0c47efdbe17262d4ea0f7926
Willow <will09600@gmail.com>
2022-03-31 15:24:27 +0200

    Add Base64Encode

--- APIHelper/Hash.cs
+++ APIHelper/Hash.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace APIHelper
 {
-    internal class Hash
+    public class Hash
     {
         public static string CalculateMD5(string filename)
         {
             using var md5 = MD5.Create();
             using var stream = File.OpenRead(filename);
             return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
+        }
+
+        public static string CalculateBase64(string input)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
         }
     }
 }
